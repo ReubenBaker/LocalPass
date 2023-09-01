dd0b217794b242716afbb44ccadaf40a51bb4f70
ReubenBaker <baker.reuben@outlook.com>
2023-09-01 17:20:38 +0100

    Setup note model

---
+++ LocalPass/Models/NoteModel.swift
@@ -0,0 +1,41 @@
+//
+//  NoteModel.swift
+//  LocalPass
+//
+//  Created by Reuben on 01/09/2023.
+//
+
+import Foundation
+
+struct Note: Identifiable, Equatable {
+    var title: String
+    var body: String
+    let creationDateTime: Date
+    var updatedDateTime: Date? = nil
+    var starred: Bool
+    
+    // Identifiable
+    var id: String {
+        title + String(creationDateTime.description) // Change later
+    }
+    
+    // Equatable
+    static func == (lhs: Note, rhs: Note) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    init(title: String, body: String) {
+        self.title = title
+        self.body = body
+        self.creationDateTime = Date()
+        self.starred = false
+    }
+    
+    init(title: String, body: String, creationDateTime: Date, updatedDateTime: Date?, starred: Bool) {
+        self.title = title
+        self.body = body
+        self.creationDateTime = creationDateTime
+        self.updatedDateTime = updatedDateTime
+        self.starred = starred
+    }
+}
