e07841cfa01ab5da1cc2c0de0d031021eb6df857
ReubenBaker <baker.reuben@outlook.com>
2023-09-18 15:22:31 +0100

    Added GPLv3 license to project

--- LocalPass/LocalPassApp.swift
+++ LocalPass/LocalPassApp.swift
@@ -7,6 +7,23 @@
 
 import SwiftUI
 
+/**
+ LocalPass
+     Copyright (C) 2023 - Reuben Baker
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 @main
 struct LocalPassApp: App {
     
